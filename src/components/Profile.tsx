4bc2d06f61996799093cb9a6c9d06db6a6e83e70
Bruna Goncalves <brunagoncalves.sjb@gmail.com>
2021-02-24 09:27:02 -0300

    Initial commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://avatars.githubusercontent.com/u/34239606?s=460&u=c68b1d5f2c05826766f3555c9948eea290edc468&v=4" alt="Bruna Goncalves"/>
+      <div>
+        <strong>Bruna Goncalves</strong>
+        <p><img src="icons/level.svg" alt="Level"/> Level 1</p>
+      </div>
+    </div>
+  )
+}
